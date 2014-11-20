06e1cc2f556a1d07d8b5f7e2bfba1178ed352e5a
Michael Piersanti <mpiersanti7@gmail.com>
2014-11-19 23:10:11 -0500

    Connection Info
    Has a possible fix to errors being thrown when running mutiple Queries.

---
+++ MongoDBConn.php
@@ -0,0 +1,21 @@
+<?PHP
+//Database Connection Info
+	try{
+			$connection = new MongoClient( "mongodb://database:csc400@ds039960.mongolab.com:39960/csc400" ); // connect to a remote host at a given port
+			$db = $connection->csc400;
+			$collection = $db->selectCollection('VRMusers');
+	} catch( Exception $e ) {
+    // This should Fix the Pooling Error on connections
+    $MaxRetries = 5;
+    for( $Counts = 1; $Counts <= $MaxRetries; $Counts ++ ) {
+        try {
+			$connection = new MongoClient( "mongodb://database:csc400@ds039960.mongolab.com:39960/csc400" ); // connect to a remote host at a given port
+			$db = $connection->csc400;
+			$collection = $db->selectCollection('VRMusers');
+        } catch( Exception $e ) {
+            continue;
+        }
+        return;
+    }
+}
+?>
